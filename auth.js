590498bbcb8a1d3dc15eeb24931233e29bb31d69
josedeweb <josealvarez712@gmail.com>
2014-02-18 11:20:51 -0500

    log passport

--- auth.js
+++ auth.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose'),
     User = mongoose.model('User');
 
 exports.twitter = function(token, tokenSecret, profile, done) {
+  console.log("usuario de twitter "+profile);
   User.findOrCreate({
     username: profile.username,
     avatar: profile._json.profile_image_url_https,
@@ -14,6 +15,7 @@ exports.twitter = function(token, tokenSecret, profile, done) {
 };
 
 exports.facebook = function(accessToken, refreshToken, profile, done) {
+  console.log("usuario de facebook "+profile);
   User.findOrCreate({
     username: profile.username,
     avatar: 'https://graph.facebook.com/'+profile.username+'/picture',
--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 /* Importamos base de datos mongodb con mongoose*/
 var mongoose = require('mongoose');
+var request = require('request');
 /* Tomamos todos los datos de usuario para guardarlos en base de datos */
 var userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
@@ -20,11 +21,24 @@ userSchema.statics.findOrCreate = function (profile, done) {
   this.findOne({ redId: profile.redId }, function (err, user) {
     if(err) return done(err);
 
-    if(user) {
-      console.log(user);
-      user.update({redId: user.Id},{username:profile.username, avatar:profile.avatar}, function(err){
-        if(err) return done(err);
+    if(user) {      
+      request(user.avatar, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          console.log("avatar bueno");        
+        }
+        else{
+          console.log("avatar malo :(");
+          if(red=="facebook"){
+
+          }
+          else{
+
+          }          
+        }
       });
+      /*user.update({redId: user.Id},{username:profile.username, avatar:profile.avatar}, function(err){
+        if(err) return done(err);
+      });*/
       return done(null, user);
     }
 
